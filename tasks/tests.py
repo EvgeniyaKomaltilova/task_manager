b382c786ed0cafb810edcd2b892e288301447530
Evgeniya Komaltilova <splendidcat@yandex.ru>
2020-09-29 19:50:02 +0700

    'History' model test added

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,5 +1,4 @@
-from datetime import date, timedelta
-
+from datetime import date, timedelta, datetime
 from django.contrib.auth.models import User
 from django.test import TestCase
 from tasks.models import Task
@@ -9,7 +8,7 @@ class TaskModelTest(TestCase):
     """Task model testing"""
 
     def test_task_object_was_created(self):
-        """test: task model was created"""
+        """test: task object was created"""
         user = User.objects.create()
         task1 = Task.objects.create(
             title='title',
@@ -28,3 +27,26 @@ class TaskModelTest(TestCase):
         self.assertEqual(task1.planned_completion_date, date.today() + timedelta(days=10))
 
 
+class HistoryModelTest(TestCase):
+    """History model testing"""
+
+    def test_task_object_was_created(self):
+        """test: history object created when the task object is saved"""
+        user = User.objects.create()
+        task = Task.objects.create(
+            title='title',
+            description='description',
+            status='new',
+            planned_completion_date=date.today() + timedelta(days=10),
+            user=user
+        )
+        task.title = 'another title'
+        task.description = 'another description'
+        task.status = 'planned'
+        task.planned_completion_date = date.today() + timedelta(days=20)
+        task.save()
+        history1 = History.objects.last()
+        self.assertEqual(history1.title, 'another title')
+        self.assertEqual(history1.description, 'another description')
+        self.assertEqual(history1.status, 'planned')
+        self.assertEqual(history1.planned_completion_date, date.today() + timedelta(days=20))
